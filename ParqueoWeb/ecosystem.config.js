f8791bcaa589cfdb0c6a9f495218daa4edfa108c
Cristian David Alvarado Lopez <crisalva5@gmail.com>
2020-08-27 21:15:50 -0600

    Update ecosystem.config.js

--- ParqueoWeb/ecosystem.config.js
+++ ParqueoWeb/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = { 
     apps: [{
       name    : "ParqueoWeb",
-      script  : "npm",
-      args    : "start"
+      script  : "ng",
+      args    : "serve --host=0.0.0.0 --disable-host-check"
     }]
   }
