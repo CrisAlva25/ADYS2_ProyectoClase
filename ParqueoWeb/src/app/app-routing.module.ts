2b55df4364fdbaa91f154c6b2537c64fc8beca2b
Losajhonny <losajhonny@gmail.com>
2020-08-28 10:17:57 -0600

    login funcionando bien

--- ParqueoWeb/src/app/app-routing.module.ts
+++ ParqueoWeb/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PerfilUsuarioComponent } from "./perfil-usuario/perfil-usuario.component";
 import { LoginParqueoAdminComponent } from './login-parqueo-admin/login-parqueo-admin.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'perfil-usuario', component: PerfilUsuarioComponent },
   { path: 'login-parqueo-admin', component: LoginParqueoAdminComponent },
+  { path: 'login-usuario-regular', component: LoginComponent },
   { path: 'admin', component: AdminComponent}
 ];
 
