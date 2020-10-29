0ecbe34d7b508cbd1c4429f7a2fc8a75de64ace0
LuisYela <javyela@gmail.com>
2020-10-28 23:50:37 -0600

    bug en rama develop de daschborad

--- ParqueoWeb/src/app/dashboard/dashboard.component.ts
+++ ParqueoWeb/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef, ɵConsole } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { RestService } from '../servicios/rest.service';
 import { AngularFireStorage } from '@angular/fire/storage';
-//import { NotifierService } from 'angular-notifier';
+import { NotifierService } from 'angular-notifier';
 import { finalize } from "rxjs/operators"
 
-const REQUEST_ADDRESS = 'all-parking-accepted';
+const REQUEST_ADDRESS = 'all-parking';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,20 +12,10 @@ const REQUEST_ADDRESS = 'all-parking-accepted';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  // alertas
-  //private readonly notifier: NotifierService;
-
-  noticia = {
-    photos: [], // Las fotos que suba el usuario van aqui
-    text: '', // el texto que agregue el usuario
-    user: '', // aqui se guarda el usuario que publico la noticia
-    parkingLot: '' // aqui se guarda el codigo del parqueo sobre el cual es la noticia 
-  };
-
-  newsPhotos = [];
+  private readonly notifier: NotifierService;
 
   currentLot = {
-    idUser: '1',
+    id: 'lasdkfnasd',
     idParking: 'asa',
     photo: 'https://cdn.ticbeat.com/src/uploads/2018/02/vender-fotos-por-internet-810x540.jpeg',
     name: '',
@@ -36,47 +26,58 @@ export class DashboardComponent implements OnInit {
     tariff: '',
     numberAvailable: 100,
     numberCapacity: 100,
+    block: false,
+    authorized: true,
     photos: [ 'https://cdn.ticbeat.com/src/uploads/2018/02/vender-fotos-por-internet-810x540.jpeg', 'https://cdn.ticbeat.com/src/uploads/2018/02/vender-fotos-por-internet-810x540.jpeg']
   };
 
   parqueos = [];
-  //parqueos = [this.currentLot, this.currentLot];//, this.currentLot, this.currentLot, this.currentLot, this.currentLot, this.currentLot];
-  
-  //esta es una auxiliar para las busquedas
-  auxParqueos = []
-  filter = '';
+  textoNoticia='';
+  //[this.currentLot, this.currentLot, this.currentLot, this.currentLot, this.currentLot, this.currentLot, this.currentLot];
 
-  //esta variable nos sirve para ir cambiando de foto en foto
   currentPhoto = 0;
+  noticia = {
+    photos: [], // Las fotos que suba el usuario van aqui
+    text: '', // el texto que agregue el usuario
+    user: '', // aqui se guarda el usuario que publico la noticia
+    parkingLot: '' // aqui se guarda el codigo del parqueo sobre el cual es la noticia 
+  } 
 
-  
+  newsPhotos = [];
+
 
 
   //esta variable sirve para controlar el modal
   @ViewChild('infoClose', { static: false }) infoClose: ElementRef;
   @ViewChild('newsClose', { static: false }) newsClose: ElementRef;
 
-  constructor(//notifierService: NotifierService,
-    public rest: RestService,
-    private storage: AngularFireStorage)
-  { 
-    this.storage = storage;
-    //this.notifier = notifierService;
+  constructor(notifierService: NotifierService,
+    private rest: RestService,
+    private storage: AngularFireStorage) {
+    let observer = this.rest.GetRequest(REQUEST_ADDRESS).subscribe( res => {
+      res.forEach(user => {
+        if (user.block === false) {
+          this.parqueos.push(user);
+        }
+        //console.log(user);
+      });
+      console.log(sessionStorage.getItem('user'));
+      //console.log(this.parqueos);
+    });
 
+    this.notifier = notifierService;
   }
 
+ 
+
   ngOnInit(): void {
-    
     //this.getParqueos();
-    for (let x = 0; x < 10; x++) {
-      this.currentLot.location = "Zona "+x;
-      this.parqueos.push(this.currentLot)
-    }
   }
 
   async getParqueos() {
     try {
       this.parqueos = await this.rest.GetRequest(REQUEST_ADDRESS).toPromise();
+      this.parqueos = this.parqueos.filter(parqueo => !parqueo.block && parqueo.authorized);
     } catch (error) { }
   }
 
@@ -95,6 +96,11 @@ export class DashboardComponent implements OnInit {
     this.infoClose.nativeElement.click();
   }
 
+
+  changeCurrentLot(parqueo) {
+    this.currentLot = parqueo;
+  }
+
   closeNewsModal(): void {
     this.noticia.text = '';
     this.noticia.photos = [];
@@ -102,46 +108,28 @@ export class DashboardComponent implements OnInit {
     this.newsClose.nativeElement.click();
   }
 
-  changeCurrentLot(parqueo) {
-    this.currentLot = parqueo;
-  }
-
-  //Agregando imagnes
-  
-  async publish() {
-    this.noticia.parkingLot = this.currentLot.location;
-    //const user = JSON.parse(sessionStorage.getItem('user'));
-    //this.noticia.user = user.id;
-
-    //ESTA VARIABLE ES TEMPORAL Y DEBE SER BORRADA CUANDO YA TODO ESTE CONECTADO
-    // LAS DOS LINEAS DE ARRIBA DEBEN SER DESCOMENTADAS
-        this.noticia.user = this.currentLot.idUser;
-        
-    let noticiaNueva = {
-      photos: this.noticia.photos,
-      text: this.noticia.text,
-      user: this.noticia.user,
-      parkingLot: this.noticia.parkingLot
-    };
-
-
-    /* this.noticia es lo que se manda a la base de datos para que se guarde*/
-    /*  AQUI VA EL METODO PUT O POST QUE LO VA A GUARDAR EN LA BASE DE DATOS*/
-    const NOTICE_ADD_ADDRESS = "post-addParkingNoticia";
-    let observer = await this.rest.PostRequest(NOTICE_ADD_ADDRESS, noticiaNueva).subscribe(res => {
-
-      //console.log(noticiaNueva);
-      if(res.bandera === true) {
-        alert("La noticia fue agregada");
-        //this.notifier.notify('success', 'noticia agregada');
+  reportParking(): void {
+    console.log(this.currentLot);
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const json = {
+      idRegular: user.id,
+      //emailRegular: user.em,
+      idParking: this.currentLot.id
+    }
+    console.log('id de  parq '+ this.currentLot.idParking);
+    console.log('id de _id '+ this.currentLot.id);
+    console.log(json);
+    let observer = this.rest.PostRequest('post-reportParking',json).subscribe( res => {
+      console.log(res);
+      if(res.success===false){
+        alert('error: '+ 'usted ya reporto este parqueo');
+      }else{
+        alert('success: '+'parqueo reportado');
       }
-      else {
-        //this.notifier.notify('error', 'la noticia no se puede agregar');
-        alert("La noticia no se puede agregar");
-      }
-      
+      //console.log(sessionStorage.getItem('user'));
+      //console.log(this.parqueos);
     });
-    this.closeNewsModal();
+
   }
 
   loadNewsImages(event: any) {
@@ -157,21 +145,57 @@ export class DashboardComponent implements OnInit {
       reader.onload = (event) => {};
     }
 
-    //const user = JSON.parse(sessionStorage.getItem('user'));
+    const user = JSON.parse(sessionStorage.getItem('user'));
     
     /* subir las imagenes a firebase */
-    const user = { id: 1} //BORRAR DESPUES DE QUE YA ESTE TODO
     this.uploadPhotoNotice(user.id);
   }
-  
+
+  async publish() {
+    this.noticia.parkingLot = this.currentLot.id;
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    this.noticia.user = user.id;
+
+    //ESTA VARIABLE ES TEMPORAL Y DEBE SER BORRADA CUANDO YA TODO ESTE CONECTADO
+    // LAS DOS LINEAS DE ARRIBA DEBEN SER DESCOMENTADAS
+    
+    //const user = { id: 1}
+    
+    let noticiaNueva = {
+      photos: this.noticia.photos,
+      text: this.noticia.text,
+      user: this.noticia.user,
+      parkingLot: this.noticia.parkingLot
+    };
+
+    /* this.noticia es lo que se manda a la base de datos para que se guarde*/
+    /*  AQUI VA EL METODO PUT O POST QUE LO VA A GUARDAR EN LA BASE DE DATOS*/
+
+    const NOTICE_ADD_ADDRESS = "post-addParkingNoticia";
+    let observer = await this.rest.PostRequest(NOTICE_ADD_ADDRESS, noticiaNueva).subscribe(res => {
+
+      console.log(noticiaNueva);
+
+      if(res.bandera === true) {
+        this.notifier.notify('success', 'noticia agregada');
+      }
+      else {
+        this.notifier.notify('error', 'la noticia no se puede agregar');
+      }
+      
+    });
+
+    this.closeNewsModal();
+  }
+
   uploadPhotoNotice(id: any) {
     let i = 1;
     this.newsPhotos.forEach(photo => {
-      //console.log("estoy entrando");
+      console.log("estoy entrando");
       let filePath = `noticia/${i}_${id}_${new Date().getTime()}`;
-      //console.log("2");
+      console.log("2");
       const fileRef = this.storage.ref(filePath);
-      //console.log("3");
+      console.log("3");
       try {
         this.downloadPhotoNotice(fileRef, filePath, photo);
       }
@@ -180,11 +204,13 @@ export class DashboardComponent implements OnInit {
       }
       i++;
     });
-    //console.log("estoy saliendo");
+    console.log("estoy saliendo");
   }
+
   downloadPhotoNotice(fileRef: any, filePath: any, photo: any): void {
     this.storage.upload(filePath, photo).snapshotChanges().pipe(
       finalize(() => {
+        console.log("4");
         fileRef.getDownloadURL().subscribe((url) => {
           this.pushPhotoNotice(url);
         })
@@ -194,6 +220,23 @@ export class DashboardComponent implements OnInit {
 
   pushPhotoNotice(url: string): void {
     this.noticia.photos.push(url);
+  }
+
+
+  makeFavorite(): void {
+    let id = this.currentLot.id;
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    /* AQUI SE ENVIA LA PETICION AL BACKEND PARA QUE SE MARQUE EL PARQUEO COMO FAVORITO DEL USUARIO */
+    const ADD_FAVORITES_ADDRESS = 'update-addFavoriteParking';
+    let info = {
+      id_user: user.id,
+      id_parking: id
+    };
+    console.log(info);
+    let observer = this.rest.PutRequest(ADD_FAVORITES_ADDRESS, info).subscribe(res => {
+      alert('SUCCESS!!! parqueo agregado a favoritos');
+      this.notifier.notify('success', 'parqueo agregado a favoritos');
+    });
   }
 
 
